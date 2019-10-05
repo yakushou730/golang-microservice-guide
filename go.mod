c08a228c3d4b18b1b4423cf83c3f2d742801a9af
Shou Tseng <yakushou730@gmail.com>
2019-10-05 17:16:22 +0800

    8. Unit tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang-microservice-guide
 
 go 1.12
+
+require github.com/stretchr/testify v1.4.0
---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,33 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	// Initiation:
+
+	// Execution:
+	user, err := GetUser(0)
+
+	// Validation:
+	assert.Nil(t, user, "we were not expecting a user with id 0")
+	assert.NotNil(t, err, "we were expecting an error when use id is 0")
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 does not exist", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	assert.EqualValues(t, 123, user.Id)
+	assert.EqualValues(t, "Shou", user.FirstName)
+	assert.EqualValues(t, "Tseng", user.LastName)
+	assert.EqualValues(t, "yakushou730@gmail.com", user.Email)
+}
