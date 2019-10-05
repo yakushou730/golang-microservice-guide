20a72a5216aaffbd024dd8d35bc237ad570218dd
Shou Tseng <yakushou730@gmail.com>
2019-10-06 01:29:27 +0800

    10. How to structure Go artifacts and mock

--- mvc/services/users_service.go
+++ mvc/services/users_service.go
@@ -5,6 +5,16 @@ import (
 	"golang-microservice-guide/mvc/utils"
 )
 
-func GetUser(userId int64) (*domain.User, *utils.ApplicationError) {
-	return domain.GetUser(userId)
+type usersService struct{}
+
+var (
+	UsersService usersService
+)
+
+func (u *usersService) GetUser(userId int64) (*domain.User, *utils.ApplicationError) {
+	user, err := domain.UserDao.GetUser(userId)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
